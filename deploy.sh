3af763a699142af3c6b37a01e1c46709f00a9f1c
dickson21 <57210266+dickson21@users.noreply.github.com>
2020-06-10 19:42:55 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 cd initial/target
 #java weblogic.Deployer -adminurl http://localhost:7001 -user weblogic -password lisadm123 -name serving-web-content -deploy -source serving-web-content-0.0.8.war
 
-java weblogic.Deployer -adminurl http://localhost:7001 -user weblogic -password lisadm123 -name serving-web-content -deploy -source serving-web-content-0.0.8.war
+java -cp /home/lisadmin/weblogic/wls12214/wlserver/server/lib/weblogic.Deployer -adminurl http://localhost:7001 -user weblogic -password lisadm123 -name serving-web-content -deploy -source serving-web-content-0.0.8.war
