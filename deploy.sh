9a7e3c01b42dcd551ef7cdd862c422739db9a89c
dickson21 <57210266+dickson21@users.noreply.github.com>
2020-06-10 17:00:16 +0800

    Create deploy.sh
    -

---
+++ deploy.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cd initial/target
+java weblogic.Deployer -adminurl http://localhost:7001 -user weblogic -password lisadm123 -name serving-web-content -deploy -source serving-web-content-0.0.8.war
