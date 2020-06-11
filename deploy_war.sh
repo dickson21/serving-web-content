e7a83ad5b2b6251007a6374d2b17da0de26154e6
dickson21 <57210266+dickson21@users.noreply.github.com>
2020-06-11 09:05:50 +0800

    Delete deploy_war.sh

--- deploy_war.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-cd initial/target
-java weblogic.Deployer -adminurl http://localhost:7001 -user weblogic -password lisadm123 -name serving-web-content -deploy -source serving-web-content-0.0.8.war
