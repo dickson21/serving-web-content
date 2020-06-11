ac5a8255b8ba62ad043dd7c1204e0a17f9ef47ee
dickson21 <57210266+dickson21@users.noreply.github.com>
2020-06-11 08:44:10 +0800

    Update deploy.sh
    deploy script update with classpath variable

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cd initial/target
 #java weblogic.Deployer -adminurl http://localhost:7001 -user weblogic -password lisadm123 -name serving-web-content -deploy -source serving-web-content-0.0.8.war
+#java -cp /home/lisadmin/weblogic/wls12214/wlserver/server/lib/weblogic.jar weblogic.Deployer -adminurl http://localhost:7001 -user weblogic -password lisadm123 -name serving-web-content -deploy -source serving-web-content-0.0.8.war
 
-java -cp /home/lisadmin/weblogic/wls12214/wlserver/server/lib/weblogic.jar weblogic.Deployer -adminurl http://localhost:7001 -user weblogic -password lisadm123 -name serving-web-content -deploy -source serving-web-content-0.0.8.war
+java -cp $CLASSPATH weblogic.Deployer -adminurl http://localhost:7001 -user weblogic -password lisadm123 -name serving-web-content -deploy -source serving-web-content-0.0.8.war
